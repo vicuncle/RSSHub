cd96a855a9ea330e6357be759b542e7106aeb00f
vicuncle <2511963350@qq.com>
2025-06-10 17:00:30 +0800

    update vercel config and add vercel entry

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,16 +1,3 @@
-const path = require('node:path');
-const moduleAlias = require('module-alias');
-moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
+import app from '@/app';
 
-const { setConfig } = require('../lib/config');
-setConfig({
-    NO_LOGFILES: true,
-});
-
-const { handle } = require('hono/vercel');
-const app = require('../lib/app');
-const logger = require('../lib/utils/logger');
-
-logger.info(`🎉 RSSHub is running! Cheers!`);
-
-module.exports = handle(app);
+export default app.fetch;
